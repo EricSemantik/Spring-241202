3ccef6e8426dfb167038667dd36436bac08265c7
AzureAD\EricSULTAN <eric@fifox.fr>
2024-12-02 12:28:49 +0100

    Tp : Correction Spring Java

--- TPS/tp-spring-start/src/main/java/spring/formation/config/ApplicationConfig.java
+++ TPS/tp-spring-start/src/main/java/spring/formation/config/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package spring.formation.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +7,10 @@ import spring.formation.orchestre.Guitare;
 import spring.formation.orchestre.Guitariste;
 import spring.formation.orchestre.IInstrument;
 import spring.formation.orchestre.IMusicien;
+import spring.formation.orchestre.Pianiste;
+import spring.formation.orchestre.Piano;
+import spring.formation.orchestre.Synthe;
+import spring.formation.orchestre.Ukulele;
 
 @Configuration
 public class ApplicationConfig {
@@ -18,7 +21,31 @@ public class ApplicationConfig {
 	}
 	
 	@Bean
-	public IMusicien guitariste(IInstrument guitare) {
-		return new Guitariste();
+	public IMusicien guitariste(Guitare guitare) {
+		return new Guitariste(guitare, "We are a champion");
+	}
+	
+	@Bean
+	public IInstrument piano() {
+		return new Piano();
+	}
+	
+	@Bean
+	public IInstrument synthe() {
+		return new Synthe();
+	}
+	
+	@Bean
+	public IInstrument ukulele() {
+		return new Ukulele();
+	}
+	
+	@Bean
+	public IMusicien pianiste(Piano piano) {
+		Pianiste pianiste = new Pianiste();
+		pianiste.setInstrument(piano);
+		pianiste.setMorceau("5ème Symphonie");
+		
+		return pianiste;
 	}
 }
