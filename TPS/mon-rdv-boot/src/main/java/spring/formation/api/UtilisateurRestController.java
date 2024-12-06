0d16ae1540fe1390498043bd6c1297331d500ae4
AzureAD\EricSULTAN <eric@fifox.fr>
2024-12-06 16:31:01 +0100

    Exemple d'authentification JWT avec Spring

--- TPS/mon-rdv-boot/src/main/java/spring/formation/api/UtilisateurRestController.java
+++ TPS/mon-rdv-boot/src/main/java/spring/formation/api/UtilisateurRestController.java
@@ -5,6 +5,9 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,7 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.validation.Valid;
+import spring.formation.api.request.ConnexionRequest;
 import spring.formation.api.request.UtilisateurRequest;
+import spring.formation.api.response.ConnexionResponse;
+import spring.formation.config.jwt.JWTUtils;
 import spring.formation.model.Role;
 import spring.formation.model.Utilisateur;
 import spring.formation.repository.IUtilisateurRepository;
@@ -31,9 +37,15 @@ public class UtilisateurRestController {
 
 	private IUtilisateurRepository utilisateurRepository;
 
-	public UtilisateurRestController(IUtilisateurRepository utilisateurRepository) {
+	// Par défaut, ce manager n'existe pas dans le contexte, donc on le configure
+	// dans SecurityConfig
+	private AuthenticationManager authenticationManager;
+
+	public UtilisateurRestController(IUtilisateurRepository utilisateurRepository,
+			AuthenticationManager authenticationManager) {
 		super();
 		this.utilisateurRepository = utilisateurRepository;
+		this.authenticationManager = authenticationManager;
 	}
 
 	@GetMapping("")
@@ -47,33 +59,36 @@ public class UtilisateurRestController {
 	}
 
 	@PostMapping("")
-	public ResponseEntity<Utilisateur> create(@RequestBody @Valid UtilisateurRequest utilisateurRequest, BindingResult result) {
-		if(utilisateurRequest.getPassword() != null && !utilisateurRequest.getPassword().equals(utilisateurRequest.getConfirmPassword()) ) {
-			result.rejectValue("confirmPassword", "utilisateur.confirPassword", "La confirmation du mot de passe est incorrecte");
+	public ResponseEntity<Utilisateur> create(@RequestBody @Valid UtilisateurRequest utilisateurRequest,
+			BindingResult result) {
+		if (utilisateurRequest.getPassword() != null
+				&& !utilisateurRequest.getPassword().equals(utilisateurRequest.getConfirmPassword())) {
+			result.rejectValue("confirmPassword", "utilisateur.confirPassword",
+					"La confirmation du mot de passe est incorrecte");
 		}
-		
-		if(result.hasErrors()) {
+
+		if (result.hasErrors()) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Utilisateur invalide !!!");
 		}
-		
+
 		Utilisateur utilisateur = new Utilisateur();
 		utilisateur.setIdentifiant(utilisateurRequest.getLogin());
 		utilisateur.setMotDePasse(utilisateurRequest.getPassword());
 		utilisateur.setActive(true);
-		
-		if(utilisateurRequest.isAdmin()) {
+
+		if (utilisateurRequest.isAdmin()) {
 			utilisateur.getRoles().add(Role.ADMIN);
 		}
-		if(utilisateurRequest.isPatient()) {
+		if (utilisateurRequest.isPatient()) {
 			utilisateur.getRoles().add(Role.PATIENT);
 		}
-		if(utilisateurRequest.isPraticien()) {
+		if (utilisateurRequest.isPraticien()) {
 			utilisateur.getRoles().add(Role.PRATICIEN);
 		}
-		if(utilisateurRequest.isSecretaire()) {
+		if (utilisateurRequest.isSecretaire()) {
 			utilisateur.getRoles().add(Role.SECRETAIRE);
 		}
-		
+
 		return new ResponseEntity<>(utilisateurRepository.save(utilisateur), HttpStatus.CREATED);
 	}
 
@@ -108,5 +123,30 @@ public class UtilisateurRestController {
 		}
 
 		utilisateurRepository.deleteById(id);
+	}
+
+	@PostMapping("/connexion")
+	public ConnexionResponse connexion(@RequestBody ConnexionRequest connexionRequest) {
+		// On va demander à SPRING SECURITY de vérifier le username / password
+		// On a besoin d'un AuthenticationManager
+		// On utilisera la méthode authenticate, qui attend un Authentication
+		// Et on utilisera le type UsernamePasswordAuthenticationToken pour donner le
+		// username & le password
+		Authentication authentication = new UsernamePasswordAuthenticationToken(connexionRequest.getUsername(),
+				connexionRequest.getPassword());
+
+		// On demande à SPRING SECURITY de vérifier ces informations de connexion
+		this.authenticationManager.authenticate(authentication);
+
+		// Si on arrive ici, c'est que la connexion a fonctionné
+		ConnexionResponse response = new ConnexionResponse();
+
+		// On génère un jeton pour l'utilisateur connecté
+		String token = JWTUtils.generate(authentication);
+
+		response.setSuccess(true);
+		response.setToken(token); // On donne le jeton en réponse
+
+		return response;
 	}
 }
--- TPS/mon-rdv-boot/src/main/java/spring/formation/config/SecurityConfig.java
+++ TPS/mon-rdv-boot/src/main/java/spring/formation/config/SecurityConfig.java
@@ -2,22 +2,28 @@ package spring.formation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import spring.formation.config.jwt.JwtHeaderAuthorizationFilter;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http, JwtHeaderAuthorizationFilter jwtFilter) throws Exception {
 		http.httpBasic(Customizer.withDefaults());
 
 		http.authorizeHttpRequests(authorize -> {
+			authorize.requestMatchers("/api/utilisateur/connexion").anonymous(); // Autorisé à tout le monde
 			authorize.requestMatchers("/api/utilisateur/**").hasRole("ADMIN");
 			authorize.requestMatchers("/api/**").authenticated();
 			authorize.requestMatchers("/h2-console/**").permitAll();
@@ -26,12 +32,22 @@ public class SecurityConfig {
 
 		http.csrf(c -> c.ignoringRequestMatchers("/api/**", "/h2-console/**"));
 
+		// Positionner le filtre JWT AVANT le filter
+		// UsernamePasswordAuthenticationFilter
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
 		return http.build();
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	// Grace à ce Bean, on pourra injecter un AuthenticationManager directement
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 
 //	@Bean
